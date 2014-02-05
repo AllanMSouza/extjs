1e342afb2524c8e6472149af35581e638f0b09ae
AllanMSouza <allan-ms@hotmail.com>
2014-02-05 18:06:47 -0200

    alterações Layout

--- app/view/layout/DataViewNewHeaderPanelCliente.js
+++ app/view/layout/DataViewNewHeaderPanelCliente.js
@@ -11,12 +11,13 @@ Ext.define('AppName.view.layout.DataViewNewHeaderPanelCliente',{
         '<tpl for=".">',
             '<div class="thumb-wrap" >',
                 '<div class="thumb" style= "">',
-                    (!Ext.isIE6? '<img align=top src="resources/imagens/iconsHeaderPanelCliente/{icon}" style="width: 64px; height: 64px;"/>':               
+//                    (!Ext.isIE6? '<ul id="menutt"> <li> <a href="#"> <img align=top src="resources/imagens/iconsHeaderPanelCliente/{icon}" style="width: 64px; height: 64px;"/><span>{name}</span></a></li></ul>':
+                    (!Ext.isIE6? '<img align=top src="resources/imagens/iconsHeaderPanelCliente/{icon}" style="width: 64px; height: 64px;"/>':                              
                     //'<div style="width:76px;height:76px;filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(src=\'app/data/php/Produtos.php?action=getImagemProdutos&id_produtos={id_produtos} style="width: 100px; height: 90px; padding: 5px;"\')"></div>
                 ''),
                 
                 '</div>',
-//                '<span><label style =" font-size:10;color:#333; width: 64px;">{name}</label></span>',
+//                '<span><label style ="float: left;    margin: 2px;    margin-right: 0;padding-left: 0px;padding-right: 80px; font-size:10;color:#333; width: 150px;">{name}</label></span>',
             '</div>',
         '</tpl>'
     ],
--- index.php
+++ index.php
@@ -7,11 +7,12 @@
         
          <link rel="stylesheet" type="text/css" href="extjs/examples/build/KitchenSink/ext-theme-neptune/lib/prettify/prettify.css"/>
           <link rel="stylesheet" href="extjs/examples/build/KitchenSink/ext-theme-neptune/resources/KitchenSink-all.css">
-          <!--<link rel="stylesheet" href="extjs/examples/build/KitchenSink/ext-theme-neptune/resources/KitchenSink-example.css">-->
+          <link rel="stylesheet" href="extjs/examples/build/KitchenSink/ext-theme-neptune/resources/KitchenSink-example.css">
           <!--<link rel="stylesheet" href="extjs/resources/css/clifton.css">-->
           <!--<link rel="stylesheet" href="extjs/resources/css/ext-all-gray.css">-->
           <!--<link rel="stylesheet" href="resources/themes/css/ext-all.css">-->
           <link rel="stylesheet" type="text/css" href="resources/css/imagens.css">
+          <link rel="stylesheet" type="text/css" href="resources/css/tooltip.css">
           <!--<link rel="stylesheet" type="text/css" href="extjs/examples/layout-browser/layout-browser.css">-->
           <link rel="stylesheet" type="text/css" href="app/view/dataview/organizer.css">
           <link rel="stylesheet" type="text/css" href="extjs/examples/shared/example.css">
