33599f5caa35af5573233fdd8a3f2e32bd4b820b
allan <allan@shitless.(none)>
2014-05-23 16:16:02 -0300

    logo removido

--- app/view/layout/ContentPanel.js
+++ app/view/layout/ContentPanel.js
@@ -8,44 +8,44 @@ Ext.define('AppName.view.layout.ContentPanel',{
     split:true,
     border: false,    
     items:[
-        {
-            xtype:'panel',
-            id:'panelLogo',
-            height: 220,
-            bodyPadding: '10',
-            border:false,
-            split: true,
-            layout: 'column',
-            y: 300,
-//            icon: 'resources/icons/logomercado.png',
-//            bodyStyle: '<img align=top src="resources/icons/logomercado.png" style="width: 64px; height: 64px;"/>'
-            items:[
-                {
-                    xtype: 'panel',
-//                    border: false,
-                    columnWidth: 0.5,
-                     bodyStyle: 'border-color:#fff;',
+//         {
+//             xtype:'panel',
+//             id:'panelLogo',
+//             height: 220,
+//             bodyPadding: '10',
+//             border:false,
+//             split: true,
+//             layout: 'column',
+//             y: 300,
+// //            icon: 'resources/icons/logomercado.png',
+// //            bodyStyle: '<img align=top src="resources/icons/logomercado.png" style="width: 64px; height: 64px;"/>'
+//             items:[
+//                 {
+//                     xtype: 'panel',
+// //                    border: false,
+//                     columnWidth: 0.5,
+//                      bodyStyle: 'border-color:#fff;',
                     
-                },
-                {
-                    xtype: 'image',
-                    id: 'logoAquiMercado',
-                    autoShow: true,
-                    split: true,
-                    width: 250,
-                    height: 200,
-                    src: 'resources/icons/logomercado.png',
-                    listeners:{
-                        render:function(val){
-                            console.log(Ext.getCmp('panelLogo').getViewRegion())
-                            Ext.getCmp('logoAquiMercado').center()
-                        }
-                    }
+//                 },
+//                 {
+//                     xtype: 'image',
+//                     id: 'logoAquiMercado',
+//                     autoShow: true,
+//                     split: true,
+//                     width: 250,
+//                     height: 200,
+//                     src: 'resources/icons/logomercado.png',
+//                     listeners:{
+//                         render:function(val){
+//                             console.log(Ext.getCmp('panelLogo').getViewRegion())
+//                             Ext.getCmp('logoAquiMercado').center()
+//                         }
+//                     }
                     
                     
-                },
-            ]
-        }
+//                 },
+//             ]
+//         }
     ]
       
 })
